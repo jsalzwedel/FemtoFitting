a4674934c2bf84cd1e532a6b7e94edecb633aa08
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-06-30 14:33:10 -0400

    Implemented general structure of main() Implemented several Fitter functions

--- Fitter.cxx
+++ Fitter.cxx
@@ -2,13 +2,98 @@
 // Fitting correlation functions
 //**********************************************************
 
+#include "Fitter.h"
+
 int main(int argc, char **argv)
 {
-  //Determine how many 
+  
+  Fitter myFitter();
+  myFitter.CreateLednickyEqns();
+  myFitter.SetFitOptions();
+
+  myFitter.CreateMinuit(/* */);
+  myFitter.DoFitting();
+  myFitter.SaveOutputPlots();
+  
+  
+}
+
+Fitter::Fitter()
+{
 
 }
 
-void SetFitOptions()
+Fitter::~Fitter()
+{
+
+}
+
+
+void Fitter::CreateLednickyEqns()
+{
+  // Create all the lednicky eqn objects that will be used
+  // in the fitting
+
+}
+
+void Fitter::CreateMinuit(/* */)
+{
+  // Create a TMinuit object.
+  // Define, set and fix parameters.
+
+  Int_t nParameters = 0;
+
+  // Using the Fit Options and the number of LednickyEqns,
+  // determine how many fit parameters there will be.
+
+  fMinuit = new TMinuit(nParameters);
+  fMinuit->SetFCN(SetParametersAndFit);
+
+  // Define the fit parameters
+  // for(int iPar = 0; iPar < nParameters; iPar++){
+  //   fMinuit->DefineParameter(iPar, parNames[iPar], parInit[iPar],  0.001, parMin[iPar],  parMax[iPar]);
+  //   if(parIsFixed[iPar]) myMinut->FixParameter(iPar);
+  // }
+  
+
+}
+
+void Fitter::DoFitting()
+{
+  // Run the fit procedure
+  Double_t arglist[5] = {0,0,0,0,0}; //Arguments that can be passed with Minuit commands
+  Int_t errFlag = 0;
+  // arglist[0] = 1;
+  // fMinuit->mnexcm("CALL FCN", arglist, 1, errFlag);
+
+  // Set how verbose the output is (from no output at -1, to max at 3)
+  arglist[0] =0;
+  fMinuit->mnexcm("SET PRINT", arglist, 1, errFlag);
+
+  // Maybe have all output results go to a file?
+
+
+  // Run Migrad with a specified max number of calls
+  arglist[0] = fMaxMinuitCalls;
+  myMinuit->mnexcm("MIGRAD", arglist, 1, errFlag);
+
+  // If outputting to file, turn return output to terminal now
+}
+
+// void AddResidual(TString residualFileName, TString residualHistName)
+// {
+//   // Maybe makes a ResidualCorrelation object?  Object would have
+//   // its own Lednicky Eqn object?
+//   // Also, a pointer to a residual correlation histogram
+
+// }
+
+void Fitter::SaveOutputPlots()
+{
+  // Save output plots for each fit
+}
+
+void Fitter::SetFitOptions()
 {
   // Set things like:
   // -How many correlations to fit
@@ -19,30 +104,7 @@ void SetFitOptions()
   
 }
 
-void CreateLednickyEqns()
-{
-  // Create all the lednicky eqn objects that will be used
-  // in the fitting
-  // Maybe rendered obsolete by AddResidual?  Will still need
-  // to create the primary-primary correlation function.
-}
-
-TMinuit *CreateMinuit()
-{
-  // Create a TMinuit object.
-  // Define, set and fix parameters.
-
-}
-
-void AddResidual(TString residualFileName, TString residualHistName)
-{
-  // Maybe makes a ResidualCorrelation object?  Object would have
-  // its own Lednicky Eqn object?
-  // Also, a pointer to a residual correlation histogram
-
-}
-
-void SetParametersAndFit(Int_t& i, Double_t *x, Double_t &totalChisquare, Double_t *par, Int_t iflag)
+void Fitter::SetParametersAndFit(Int_t& i, Double_t *x, Double_t &totalChisquare, Double_t *par, Int_t iflag)
 {
   // Take the TMinuit parameters par, set them in the Lednicky eqns,
   // and get the resulting Chisquare of the fit.
--- Fitter.h
+++ Fitter.h
@@ -1,14 +1,32 @@
-//...
+//**********************************************************
+// Fitting correlation functions
+//**********************************************************
+
+#ifndef Fitter_H
+#define Fitter_H
+
 
 
 class Fitter{
  public:
   Fitter();
   virtual ~Fitter();
-
+  
+  void CreateLednickyEqns();
+  void CreateMinuit();
+  void DoFitting();
+  void SaveOutputPlots();
+  void SetFitOptions();
+  
  private:
   void SetParametersAndFit(Int_t& i, Double_t *x, Double_t &totalChisquare, Double_t *par, Int_t iflag);
 
   TMinuit *fMinuit;
   vector<*LednickyEqn> fLedEquations;
+  vector<TString>  parNames;
+  vector<Double_t> parInitial;
+  vector<Double_t> parMinimum;
+  vector<Double_t> parMaximum;
+  vector<Bool_t>   parIsFixed;
+  Int_t fMaxMinuitCalls;
 }
