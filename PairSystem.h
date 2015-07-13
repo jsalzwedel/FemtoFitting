1d75a1ce4cefb5bb8c909843e32ae4f8d1de5627
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-07-13 16:24:49 -0400

    Made SetLednickyParams public. Reordered function list

--- PairSystem.h
+++ PairSystem.h
@@ -14,9 +14,10 @@ class PairSystem{
  public:
   PairSystem(TH1D *cfData, Bool_t isIdenticalPrimary/*, */);
   ~PairSystem();
-  Double_t CalculateFitChisquare(vector<Double_t> pars);
-  void SetCFData(TH2D* correlationFunction);
+  Double_t CalculateFitChisquare();
   void CreateNewLednickyEqn(TString name, Bool_t isIdentical, TH2D *transformMatrix);
+  void SetCFData(TH2D* correlationFunction);
+  void SetLednickyParams(vector<Double_t> pars);
 
  private:
   TString fPairTypeName; // e.g. "LambdaLambda" or "LambdaAntilambda"
@@ -25,7 +26,7 @@ class PairSystem{
   TH1D *fCF; // Correlation function data for the pair type
   //... fit range (Double? Bin integer?)
   
-  void SetLednickyParams(vector<Double_t> pars);
+
   
   
 
