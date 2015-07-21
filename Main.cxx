533b24916fc27a612d558f04564ffba4768256cd
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-07-21 15:52:02 -0400

    Commented out the test main() used to check LednickyEqn class. Updated true main to use UserSetupSystems Updated main to make sure there is at least one system.

--- Main.cxx
+++ Main.cxx
@@ -10,31 +10,31 @@
 
 
 
-int main()
-{
+// int main()
+// {
 
-  TFile transFile("TestTransforms.root");
-  TH2D *transform = (TH2D*) transFile.Get("TestTrans1");
-  // TH2D *transform = NULL;
-  Bool_t isIdentical = false;
-  TString name = "TestRes1";
-  Int_t nBins = 200;
-  Double_t binWidth = 0.01;
-  LednickyEqn ledEqn(name, isIdentical, transform, nBins, binWidth);
-  Double_t radius = 1.;
-  Double_t rf0 = -1.;
-  Double_t if0 = 1.;
-  Double_t d0 = 3.;
-  Double_t parsArr[4] = {radius, rf0, if0, d0};
-  vector<Double_t> pars(parsArr, parsArr+4);
-  ledEqn.SetParameters(pars);
-  TGraph *myGraph = ledEqn.GetLednickyGraph();
+//   TFile transFile("TestTransforms.root");
+//   TH2D *transform = (TH2D*) transFile.Get("TestTrans1");
+//   // TH2D *transform = NULL;
+//   Bool_t isIdentical = false;
+//   TString name = "TestRes1";
+//   Int_t nBins = 200;
+//   Double_t binWidth = 0.01;
+//   LednickyEqn ledEqn(name, isIdentical, transform, nBins, binWidth);
+//   Double_t radius = 1.;
+//   Double_t rf0 = -1.;
+//   Double_t if0 = 1.;
+//   Double_t d0 = 3.;
+//   Double_t parsArr[4] = {radius, rf0, if0, d0};
+//   vector<Double_t> pars(parsArr, parsArr+4);
+//   ledEqn.SetParameters(pars);
+//   TGraph *myGraph = ledEqn.GetLednickyGraph();
 
-  TFile outfile("testFile.root", "update");
-  myGraph->Write(myGraph->GetName(), TObject::kOverwrite);
+//   TFile outfile("testFile.root", "update");
+//   myGraph->Write(myGraph->GetName(), TObject::kOverwrite);
   
-  return 0;
-}
+//   return 0;
+// }
 
 
 void UserSetupSystems(Fitter &fitter)
@@ -68,19 +68,25 @@ void UserSetupSystems(Fitter &fitter)
 }
 
 
-/*
+
 // main for full program
 int main(int argc, char **argv)
 {
   
-  // Check that the first argument (configuration number)
-  // is an integer.
-  Int_t config;
-  if (sscanf (argv[1], "%i", &config)!=1) { printf ("error - not an integer"); }
+  // // Check that the first argument (configuration number)
+  // // is an integer.
+  // Int_t config;
+  // if (sscanf (argv[1], "%i", &config)!=1) { printf ("error - not an integer"); }
 
   // Setup
   Fitter myFitter();
-  myFitter.CreateAllPairSystems(config);
+  UserSetupSystems(myFitter);
+  // Add more systems as needed, either here or in UserSetupSystems
+  if(myFitter.GetNSystems() < 1) {
+    cout<<"No systems to fit."<<endl;
+    return 1;
+  }
+  // myFitter.CreateAllPairSystems(config);
   myFitter.SetFitOptions();
 
   // Fitting
@@ -88,6 +94,6 @@ int main(int argc, char **argv)
   myFitter.DoFitting();
   myFitter.SaveOutputPlots();
   
-  
+  return 0;
 }
- */
+ 
