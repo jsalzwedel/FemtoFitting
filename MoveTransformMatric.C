4d535648550e318f407cab3afcafa3abfd67f669
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-07-22 11:34:56 -0400

    Added simple file for getting transform matrices into the right format and file

---
+++ MoveTransformMatric.C
@@ -0,0 +1,14 @@
+void MoveTransformMatric(TString inFileName, TString matrixName)
+{
+  TFile inFile(inFileName);
+  TH2D *transform = (TH2D*) inFile.Get(matrixName);
+  
+  TFile outFile("PreparedTransformMatrices.root","update");
+  if(transform) {
+    outFile.cd();
+    transform->Write(transform->GetName(), TObject::kOverwrite);
+  }
+  else cout<<"Could not find matrix with that name"<<endl;
+  outFile.Close();
+  inFile.Close();
+}
