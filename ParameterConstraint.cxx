d5e25336b878a09fe0534fb2d17d502c65fdd7c0
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-08-06 11:24:10 -0400

    Added a SortConstraints function to ParameterConstraint

--- ParameterConstraint.cxx
+++ ParameterConstraint.cxx
@@ -4,13 +4,43 @@
 
 #include "ParameterConstraint.h"
 
-ParameterConstraint::ParameterConstraint(Int_t constrainedParam, vector<Int_t> constrainedSystems)
+ParameterConstraint::ParameterConstraint(Int_t constrainedParam, vector<Int_t> constrainedSystems, const vector<PairSystem*> &pairSystems)
 {
   fParameter = constrainedParam;
   fConstrainedSystems = constrainedSystems;
+  SortConstraints(pairSystems);
 }
 
 ParameterConstraint::~ParameterConstraint()
 {
 
+}
+
+void ParameterConstraint::SortConstraints(const vector<PairSystem*> &pairSystems)
+{
+  // Sort the constraints so that they follow the same order as the pair systems
+
+  // Fill a vector with system types in the order that they appear
+  // in the pair system vector
+  vector<Int_t> systemOrder;
+  for(UInt_t iSys = 0; iSys < pairSystems.size(); iSys++)
+  {
+    systemOrder.push_back(pairSystems[iSys]->GetSystemType());
+  }
+
+  vector<Int_t> tempOrder(fConstrainedSystems);
+  Int_t entry = 0;
+  // Put the systems into fConstrainedSystem in the same order
+  // as they appear in systemorder
+  for(UInt_t iSys = 0; iSys < systemOrder.size(); iSys++)
+  {
+    for(UInt_t iTemp = 0; iTemp < tempOrder.size(); iTemp++)
+    {
+      if(systemOrder[iSys] == tempOrder[iTemp])
+      {
+	fConstrainedSystems[entry] = tempOrder[iTemp];
+	entry++;
+      }
+    }    
+  }
 }
--- ParameterConstraint.h
+++ ParameterConstraint.h
@@ -7,18 +7,20 @@
 
 #include <vector>
 #include "Rtypes.h"
+#include "PairSystem.h"
 
 using std::vector;
 
 class ParameterConstraint{
  public:
-  ParameterConstraint(Int_t constrainedParam, vector<Int_t> constrainedSystems);
+  ParameterConstraint(Int_t constrainedParam, vector<Int_t> constrainedSystems, const vector<PairSystem*> &pairSystems);
   virtual ~ParameterConstraint();
   Int_t GetConstrainedParam() {return fParameter;};
   vector<Int_t> GetConstrainedSystems() {return fConstrainedSystems;};
   
 
  private:
+  void SortConstraints(const vector<PairSystem*> &pairSystems);
   Int_t fParameter; // The index of the parameter that is constrained
   vector<Int_t> fConstrainedSystems; // Indices of the systems that share the constraint
 };
