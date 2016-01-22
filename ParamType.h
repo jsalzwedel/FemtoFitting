91f6c4a5cc0d4c37793fc26f0fc983f2f95f550f
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-01-22 12:58:30 -0500

    ParamType: Added linear and quad params

--- ParamType.h
+++ ParamType.h
@@ -1,11 +1,12 @@
 #ifndef ParamType_H
 #define ParamType_H
 
-enum ParamType  {kRad     = 0,
-		 kF0Real  = 1,
-		 kF0Imag  = 2,
-		 kD0      = 3,
-		 kBkgCoeff = 4,
-		 kNorm    = 5};
+enum ParamType  {kRad       = 0,
+		 kF0Real    = 1,
+		 kF0Imag    = 2,
+		 kD0        = 3,
+		 kLinearBkg = 4,
+		 kQuadBkg   = 5,
+		 kNorm      = 6};
 
 #endif
