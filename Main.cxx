861c833b829b2b2f4910e7cbefd27a6ebc707e00
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-05-13 12:29:15 -0400

    Added "LinearBkg" to comment about initial parameters Fiddling with params

--- Main.cxx
+++ Main.cxx
@@ -89,9 +89,9 @@ void UserSetupSystems(Fitter *fitter)
          useAA010Chi2 = kFALSE, useAA1030Chi2 = kFALSE, useAA3050Chi2 = kFALSE,
          useLA010Chi2 = kFALSE, useLA1030Chi2 = kFALSE, useLA3050Chi2 = kFALSE;
   // Uncomment these as needed
-  // useLLAA010Chi2  = kTRUE;
-  // useLLAA1030Chi2 = kTRUE;
-  // useLLAA3050Chi2 = kTRUE;
+  useLLAA010Chi2  = kTRUE;
+  useLLAA1030Chi2 = kTRUE;
+  useLLAA3050Chi2 = kTRUE;
   // useLL010Chi2    = kTRUE;
   // useLL1030Chi2   = kTRUE;
   // useLL3050Chi2   = kTRUE;
@@ -118,7 +118,7 @@ void UserSetupSystems(Fitter *fitter)
   //  // useAA010Log    = kTRUE; //Not implemented 
   //  // useAA1030Log   = kTRUE; //Not implemented
   //  // useAA3050Log   = kTRUE; //Not implemented
-  useLA010Log    = kTRUE;
+  // useLA010Log    = kTRUE;
   // useLA1030Log   = kTRUE;
   // useLA3050Log   = kTRUE;
 
@@ -139,16 +139,16 @@ void UserSetupSystems(Fitter *fitter)
   TString fileName = "/home/jai/Analysis/lambda/AliAnalysisLambda/Results/AnalysisResults/CFs.root";
   TString histName = "Merged/CFLLAA010";
   TString simpleName = "LLAA010";
-  // Make initial parameters: Radius, ReF0, ImF0, D0, QuadBkg, Norm 
+  // Make initial parameters: Radius, ReF0, ImF0, D0, LinearBkg, QuadBkg, Norm 
   Double_t radiiParams[3] = {4., 2.67, 2.02};
-  Double_t initParamsArr[kNorm + 1] = {radiiParams[0], -.63, 0., 1.36, 0., 0., 1.}; 
+  Double_t initParamsArr[kNorm + 1] = {radiiParams[0], -.63, 0., 1., 0., 0., 1.}; 
   vector<Double_t> initParams(initParamsArr, initParamsArr + kNorm + 1);
   Double_t minParamsArr[kNorm + 1] = {0., 0., 0., 0., 0., 0., 0.};
   vector<Double_t> minParams(minParamsArr, minParamsArr + kNorm + 1);
   Double_t maxParamsArr[kNorm + 1] = {0., 0., 0., 0., 0., 0., 0.};
   vector<Double_t> maxParams(maxParamsArr, maxParamsArr + kNorm + 1);  
   // Determine which parameters should be fixed in the fitter.
-  Bool_t fixParamsArr[kNorm + 1] = {kFALSE, kFALSE, kTRUE, kFALSE, !useLinearBkgPoly, !useQuadBkgPoly, kFALSE};
+  Bool_t fixParamsArr[kNorm + 1] = {kFALSE, kFALSE, kTRUE, kTRUE, !useLinearBkgPoly, !useQuadBkgPoly, kFALSE};
   vector<Bool_t> fixParams(fixParamsArr, fixParamsArr + kNorm + 1);
   // Prepare the lednicky eqn info (lambda parameters, transform matrix locations, whether or not particles are identical)
   vector<LednickyInfo> ledInfoLL = PrepareLednickyInfo(kTRUE, useRootSScalingLL);
@@ -220,9 +220,9 @@ void UserSetupSystems(Fitter *fitter)
   // histName = "mm12CF20";
 
   simpleName = "LA010";
-  Double_t initParamsArrLA[kNorm + 1] = {radiiParams[0], -1., 1., 3., 0., 0., 1.}; 
+  Double_t initParamsArrLA[kNorm + 1] = {radiiParams[0], -1., 1., 0., 0., 0., 1.}; 
   vector<Double_t> initParamsLA(initParamsArrLA, initParamsArrLA + kNorm + 1);
-  Bool_t fixParamsArrLA[kNorm + 1] = {kFALSE, kFALSE, kFALSE, kFALSE, !useLinearBkgPoly, !useQuadBkgPoly, kFALSE};
+  Bool_t fixParamsArrLA[kNorm + 1] = {kFALSE, kFALSE, kFALSE, kTRUE, !useLinearBkgPoly, !useQuadBkgPoly, kFALSE};
   vector<Bool_t> fixParamsLA(fixParamsArrLA, fixParamsArrLA + kNorm + 1);
 
   vector<LednickyInfo> ledInfoLA = PrepareLednickyInfo(kFALSE, useRootSScalingLA);
@@ -614,14 +614,14 @@ void UserSetConstraints(Fitter *myFitter)
   // Share real f0, imaginary f0, and d0 among partical-antiparticle
   Int_t systemsArrLA[3] = {kLA010, kLA1030, kLA3050};
   vector<Int_t> systemsLA(systemsArrLA, systemsArrLA + 3);
-  // myFitter->SetupConstraint(kF0Real, systemsLA);
-  // myFitter->SetupConstraint(kF0Imag, systemsLA);
+  myFitter->SetupConstraint(kF0Real, systemsLA);
+  myFitter->SetupConstraint(kF0Imag, systemsLA);
   // myFitter->SetupConstraint(kD0, systemsLA);
 
   // Share real f0, imaginary f0, and d0 among LambdaLambda + AntilambdaAntilambda
   Int_t systemsArrLLAA[3] = {kLLAA010, kLLAA1030, kLLAA3050};
   vector<Int_t> systemsLLAA(systemsArrLLAA, systemsArrLLAA + 3);
-  // myFitter->SetupConstraint(kF0Real, systemsLLAA);
+  myFitter->SetupConstraint(kF0Real, systemsLLAA);
   // myFitter->SetupConstraint(kD0, systemsLLAA);
 
   // Share real f0, imaginary f0, and d0 among LambdaLambda
